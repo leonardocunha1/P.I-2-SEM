a4484f0f072b72870184272684c7d27b494c2913
Leonardo Cunha <leo.francasp97@gmail.com>
2024-08-22 16:50:39 -0300

    Acrescentei o Link na logo do site para redirecionar para home

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 import LogoImg from "/images/logos/logo4.jpg";
 
 function Logo() {
-  return <img src={LogoImg} alt="Logo" className="h-20 rounded-full" />;
+  return (
+    <Link to="/home">
+      <img src={LogoImg} alt="Logo" className="h-20 rounded-full" />
+    </Link>
+  );
 }
 
 export default Logo;
