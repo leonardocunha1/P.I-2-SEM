576fcadecec627e739325edc449be84d054640af
Leonardo Cunha <leo.francasp97@gmail.com>
2024-10-16 18:16:08 -0300

    Ajustes visuais nos botões do carrinho

--- src/features/cart/AddressForm.jsx
+++ src/features/cart/AddressForm.jsx
@@ -5,6 +5,7 @@ import { Controller, useForm } from "react-hook-form";
 import ClipLoader from "react-spinners/ClipLoader";
 import { fetchCepData } from "@/utils/data-services";
 import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 // Defina o esquema Zod para o formulário
 const addressSchema = z.object({
@@ -20,6 +21,24 @@ const addressSchema = z.object({
 const override = {
   display: "block",
   margin: "0 auto",
+};
+
+// Cor principal do tema
+const color = "#e0782f";
+
+// Estilos para o TextField
+const styleTextField = {
+  "& label.Mui-focused": {
+    color: color, // Cor do label quando focado
+  },
+  "& .MuiOutlinedInput-root": {
+    "&:hover fieldset": {
+      borderColor: color, // Cor da borda ao passar o mouse
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: color, // Cor da borda quando focado
+    },
+  },
 };
 
 function AddressForm({ setCepError, setCepData, setShowInputs }) {
@@ -59,22 +78,6 @@ function AddressForm({ setCepError, setCepData, setShowInputs }) {
       className="mt-4 flex flex-col gap-4"
       onSubmit={handleSubmit(onSubmit)}
     >
-      {/* <div className="flex flex-col gap-2">
-        <label htmlFor="cep" className="text-sm font-semibold text-stone-700">
-          CEP
-        </label>
-        <input
-          type="text"
-          id="cep"
-          placeholder="Digite o CEP"
-          className="w-full rounded-md border border-stone-300 px-2 py-1 focus:outline-none focus:ring focus:ring-primary-400 focus:ring-offset-2"
-          {...register("cep")}
-        />
-        {errors.cep && (
-          <p className="mt-1 text-xs text-red-500">{errors.cep.message}</p>
-        )}
-      </div> */}
-
       <Controller
         name="cep"
         control={control}
@@ -91,19 +94,7 @@ function AddressForm({ setCepError, setCepData, setShowInputs }) {
               errors.cep ? errors.cep.message : "Por favor, digite seu CEP"
             } // Exibir mensagem de erro ou de ajuda
             size="small"
-            sx={{
-              "& label.Mui-focused": {
-                color: "#ff9800", // Cor do label quando focado
-              },
-              "& .MuiOutlinedInput-root": {
-                "&:hover fieldset": {
-                  borderColor: "#ff9800", // Cor da borda ao passar o mouse
-                },
-                "&.Mui-focused fieldset": {
-                  borderColor: "#ff9800", // Cor da borda quando focado
-                },
-              },
-            }}
+            sx={styleTextField}
           />
         )}
       />
@@ -125,24 +116,12 @@ function AddressForm({ setCepError, setCepData, setShowInputs }) {
                 : "Por favor, digite o número"
             } // Exibir mensagem de erro ou de ajuda
             size="small"
-            sx={{
-              "& label.Mui-focused": {
-                color: "#ff9800", // Cor do label quando focado
-              },
-              "& .MuiOutlinedInput-root": {
-                "&:hover fieldset": {
-                  borderColor: "#ff9800", // Cor da borda ao passar o mouse
-                },
-                "&.Mui-focused fieldset": {
-                  borderColor: "#ff9800", // Cor da borda quando focado
-                },
-              },
-            }}
+            sx={styleTextField}
           />
         )}
       />
 
-      <button
+      {/* <button
         type="submit"
         className="flex items-center justify-center rounded-md bg-primary-500 px-4 py-2 font-bold text-primary-50 duration-200 hover:bg-primary-600"
       >
@@ -151,9 +130,49 @@ function AddressForm({ setCepError, setCepData, setShowInputs }) {
         ) : (
           "Ok"
         )}
-      </button>
+      </button> */}
+
+      <Button
+        variant="contained"
+        size="small"
+        type="submit"
+        sx={{
+          backgroundColor: "#e0782f",
+          color: "#1a1a1a",
+          fontWeight: "bold",
+          width: "100%",
+          fontFamily: "Calistoga",
+          letterSpacing: "2px",
+          "&:hover": {
+            backgroundColor: "#d16025",
+            color: "#fdf7ef",
+          },
+        }}
+      >
+        {isLoading ? (
+          <ClipLoader color="#fdf7ef" size={20} cssOverride={override} />
+        ) : (
+          "Buscar"
+        )}
+      </Button>
     </form>
   );
 }
 
 export default AddressForm;
+
+//   <div className="flex flex-col gap-2">
+//     <label htmlFor="cep" className="text-sm font-semibold text-stone-700">
+//       CEP
+//     </label>
+//     <input
+//       type="text"
+//       id="cep"
+//       placeholder="Digite o CEP"
+//       className="w-full rounded-md border border-stone-300 px-2 py-1 focus:outline-none focus:ring focus:ring-primary-400 focus:ring-offset-2"
+//       {...register("cep")}
+//     />
+//     {errors.cep && (
+//       <p className="mt-1 text-xs text-red-500">{errors.cep.message}</p>
+//     )}
+//   </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,119 +1,3 @@
-// import { useState } from "react";
-// import { useCart } from "@/contexts/CartContext";
-// import { formatCurrency } from "@/utils/helpers";
-// import CartItem from "@/features/cart/CartItem";
-// import EmptyCart from "@/features/cart/EmptyCart";
-// import TextMd from "@/ui/TextMd";
-// import AddressForm from "@/features/cart/AddressForm";
-// import PaymentMethod from "@/features/cart/PaymentMethod";
-
-// function Cart() {
-//   const [isDelivery, setIsDelivery] = useState(false);
-//   const [cepError, setCepError] = useState(null);
-//   const [cepData, setCepData] = useState(null);
-//   const [showInputs, setShowInputs] = useState(true); // controle dos inputs
-//   const [paymentMethod, setPaymentMethod] = useState("");
-
-//   const { cart } = useCart();
-//   const valorEntrega = 8;
-
-//   const totalOrder = cart.reduce((acc, item) => {
-//     return acc + item.price * item.quantity;
-//   }, 0);
-
-//   function handleReset() {
-//     setCepData(null);
-//     setShowInputs(true); // Mostrar inputs ao resetar
-//   }
-
-//   return (
-//     <section className="relative flex-1 bg-stone-50 px-5 py-10">
-//       {cart.length === 0 ? (
-//         <EmptyCart />
-//       ) : (
-//         <div className="mx-auto max-w-3xl rounded-lg bg-white px-6 py-8">
-//           <TextMd label="Carrinho" />
-//           <ul>
-//             {cart.map((item) => (
-//               <CartItem key={item.id} item={item} />
-//             ))}
-//           </ul>
-//           <div className="mt-4 space-y-2">
-//             <div className="space-y-2">
-//               <PaymentMethod
-//                 setPaymentMethod={setPaymentMethod}
-//                 paymentMethod={paymentMethod}
-//               />
-//             </div>
-
-//             <div className="flex h-[30px] items-center gap-2">
-//               <label htmlFor="delivery">
-//                 Entrega ({formatCurrency(valorEntrega)})
-//               </label>
-//               <input
-//                 type="checkbox"
-//                 id="delivery"
-//                 className="h-4 w-4 accent-primary-400 focus:outline-none focus:ring focus:ring-primary-400 focus:ring-offset-2"
-//                 checked={isDelivery}
-//                 onChange={() => {
-//                   setIsDelivery(!isDelivery);
-//                   setCepData(null);
-//                   setShowInputs(true);
-//                 }}
-//               />
-//             </div>
-
-//             {isDelivery && (
-//               <div className="max-w-96">
-//                 {showInputs ? (
-//                   <AddressForm
-//                     setCepError={setCepError}
-//                     setCepData={setCepData}
-//                     setShowInputs={setShowInputs}
-//                   />
-//                 ) : (
-//                   <>
-//                     <div className="mt-3 rounded-md border border-primary-100 bg-primary-50 p-4">
-//                       <p className="text-sm text-stone-700">
-//                         {cepData.logradouro}{" "}
-//                         {cepData.numero && `n°${cepData.numero}`} -{" "}
-//                         {cepData.bairro}
-//                       </p>
-//                     </div>
-//                     <button
-//                       type="button"
-//                       className="mt-4 rounded-md bg-primary-500 px-4 py-2 font-semibold tracking-wide text-primary-50 duration-200 hover:bg-primary-600"
-//                       onClick={handleReset}
-//                     >
-//                       Alterar CEP
-//                     </button>
-//                   </>
-//                 )}
-//                 {cepError && (
-//                   <p className="mt-2 text-sm text-red-500">{cepError}</p>
-//                 )}
-//               </div>
-//             )}
-//           </div>
-
-//           <div className="mt-2 flex items-center justify-end gap-2">
-//             <p>Total:</p>
-//             <p className="text-base font-bold">
-//               {formatCurrency(
-//                 isDelivery ? totalOrder + valorEntrega : totalOrder,
-//               )}
-//             </p>
-//           </div>
-
-//           <button>Finalizar Pedido</button>
-//         </div>
-//       )}
-//     </section>
-//   );
-// }
-
-// export default Cart;
-
 import { useState } from "react";
 import { useCart } from "@/contexts/CartContext";
 import { formatCurrency } from "@/utils/helpers";
@@ -124,6 +8,7 @@ import AddressForm from "@/features/cart/AddressForm";
 import PaymentMethod from "@/features/cart/PaymentMethod";
 import AddressSummary from "@/features/cart/AddressSumary";
 import TotalOrderDisplay from "@/features/cart/TotalOrderDisplay";
+import Button from "@mui/material/Button";
 
 function Cart() {
   const [isDelivery, setIsDelivery] = useState(false);
@@ -202,7 +87,24 @@ function Cart() {
             valorEntrega={valorEntrega}
           />
 
-          <button>Finalizar Pedido</button>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              backgroundColor: "#e0782f",
+              color: "#1a1a1a",
+              fontWeight: "bold",
+              width: "100%",
+              fontFamily: "Calistoga",
+              letterSpacing: "2px",
+              "&:hover": {
+                backgroundColor: "#d16025",
+                color: "#fdf7ef",
+              },
+            }}
+          >
+            Finalizar Pedido
+          </Button>
         </div>
       )}
     </section>
